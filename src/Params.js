3cbfd9e50424f5e908e5c5c66c9a53742debf291
o.podilko <opodilko122@gmail.com>
2025-02-04 19:59:41 +0200

    first commit

---
+++ src/Params.js
@@ -0,0 +1,7 @@
+export default class Params {
+  static keyPush = 'f985fc3e-e936-4b90-8562-3534018187fe'; // ключ OneSignal
+  static appID = '6741209508'; // ID додатку в сторі
+  static keyApps = 'BX5RmT4UeiWBs7XFeqzzDM'; // ключ Appsflyer
+  static targetDate = '2025-02-02'; // дата закінчення відльожки
+  static bodyLin = 'https://spectacular-exalted-delight.space/pfWMCVbN'; // тіло лінки з якою будемо працювати
+}
